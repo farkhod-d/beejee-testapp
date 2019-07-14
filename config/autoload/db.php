72a610a6ca863ede001b722f392d96c6c9bd8ca9
Farkhod Daniyarov <fudeglan@gmail.com>
2019-07-14 22:39:00 +0500

    Add db config

---
+++ config/autoload/db.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Local Configuration Override
+ *
+ * This configuration override file is for overriding environment-specific and
+ * security-sensitive configuration information. Copy this file without the
+ * .dist extension at the end and populate values as needed.
+ *
+ * @NOTE: This file is ignored from Git by default with the .gitignore included
+ * in ZendSkeletonApplication. This is a good practice, as it prevents sensitive
+ * credentials from accidentally being committed into version control.
+ */
+
+// use Doctrine\DBAL\Driver\PDOMySql\Driver as PDOMySqlDriver;
+// use Doctrine\DBAL\Driver\PDOSqlite\Driver as PDOSqliteDriver;
+//
+// return [
+//     'doctrine' => [
+//         'connection' => [
+//             'orm_default2' => [
+//                 'driverClass' => PDOMySqlDriver::class,
+//                 'params' => [
+//                     'host'     => 'localhost',
+//                     'port' => '3306',
+//                     'user'     => 'root',
+//                     'password' => '',
+//                     'dbname'   => 'test-beejee',
+//                     'charset' => 'utf8',
+//                 ]
+//             ],
+//             'orm_default' => [
+//                 'driverClass' => PDOSqliteDriver::class,
+//                 'params' => [
+//                      'path'=> __DIR__ . '/../../data/db.sqlite',
+//                 ]
+//             ]
+//         ],
+//     ],
+// ];
